e8ea927648c6929edc5426c0415a0262eeaea20e
tarekbeb <tariqbeb@gmail.com>
2024-07-01 15:33:14 -0500

    Adds rollup to bundle

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   parserOptions: {
     project: 'tsconfig.json',
     sourceType: 'module',
+    ecmaVersion: 2020,
   },
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'prettier'],
   plugins: ['@typescript-eslint/eslint-plugin', 'prettier'],
@@ -17,4 +18,14 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
   },
+  exclude: ['node_modules/**', 'dist/**'],
+  overrides: [
+    {
+      files: ['rollup.config.js'],
+      parser: 'espree', // Use the default JS parser for this file
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off', // Disable TS specific rules for this file
+      },
+    },
+  ],
 };
